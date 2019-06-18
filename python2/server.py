1d86d90e7fe0eca15fbf1d10619303f5b51c92e0
LucienShui <lucien@lucien.ink>
2019-06-18 17:04:20 +0800

    add asyn, add response body

--- python2/server.py
+++ python2/server.py
@@ -5,11 +5,18 @@ import os
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     '''Process & Accept requests'''
 
+    # Page template
+    Page = '''success'''
+
     # Process an GET requests
     def do_GET(self):
-        os.system("bash -x main.sh")
+        os.system("bash -x main.sh &")
         self.send_response(200)
-
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(self.Page)))
+        self.end_headers()
+        self.wfile.write(self.Page)
+        
 
 if __name__ == '__main__':
     serverAddress = ('', 10086)
--- python3/server.py
+++ python3/server.py
@@ -6,8 +6,12 @@ class RequestHandler(BaseHTTPRequestHandler):
 
     # Process an GET requests
     def do_GET(self):
-        os.system("bash -x main.sh")
+        os.system("bash -x main.sh &")
         self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.send_header("Content-Length", str(len(self.Page)))
+        self.end_headers()
+        self.wfile.write(self.Page)
 
 
 if __name__ == '__main__':
